24f6da7c37d811590d87ada68d9d204e691e01c3
eivinhoy <eivind@hoydal.net>
2019-05-03 21:48:39 +0000

    update from joint git

---
+++ Challenges/Plankton/extract_zip.py
@@ -0,0 +1,34 @@
+import zipfile
+from io import BytesIO
+from PIL import Image
+from tqdm import tqdm
+
+def extract_zip_to_memory(input_zip):
+    '''
+    This function extracts the images stored inside the given zip file.
+    It stores the result in a python dictionary.
+    
+    input_zip (string): path to the zip file
+    
+    returns (dict): {filename (string): image_file (bytes)}
+    '''
+    input_zip=zipfile.ZipFile(input_zip)
+    return {name: BytesIO(input_zip.read(name)) for name in tqdm(input_zip.namelist()) if name.endswith('.jpg')}
+
+
+# imgFiles = extract_zip_to_memory("imgs.zip")
+
+# Display an example image 
+# Image.open(imgFiles['imgs/32738710.jpg'])
+
+# Load the image as a numpy array:
+# np_arr = np.array(Image.open(imgFiles['imgs/32738710.jpg']))
+
+# Be aware that the dictionary will occupy roughly 2GB of computer memory!
+# To free this memory again, run:
+# del imgFiles
+
+'''ZipFile.read(name[, pwd])
+Return the bytes of the file name in the archive. name is the name of the file in the archive, or a ZipInfo object. The archive must be open for read or append. pwd is the password used for encrypted files and, if specified, it will override the default password set with setpassword(). Calling read() on a closed ZipFile will raise a RuntimeError.'''
+
+
